177ab449344bbbc0ed58399a6619aec03fa413a8
Max Hegler <mhegler@miva.com>
2019-12-18 15:14:45 -0800

    removed server stuff

--- client/MVT/src/extension.ts
+++ client/MVT/src/extension.ts
@@ -1,66 +1,29 @@
-import * as path from 'path';
-import { workspace, ExtensionContext, commands } from 'vscode';
 
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient';
+import * as vscode from 'vscode';
 
-let client: LanguageClient;
+export function activate( context: vscode.ExtensionContext ) {
 
-export function activate( context: ExtensionContext ) {
+	let doValueCompletionProvider = vscode.languages.registerCompletionItemProvider(
+		'mvt',
+		{
+			provideCompletionItems( document: vscode.TextDocument, position: vscode.Position ) {
 
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join( 'server', 'MVT', 'out', 'server.js' )
+				return [
+					new vscode.CompletionItem( 'test', vscode.CompletionItemKind.Method )
+				];
+
+			}
+		}
 	);
 
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: [ '--nolazy', '--inspect=6009' ] };
-
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
-
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for mvt documents
-		documentSelector: [ { scheme: 'file', language: 'mvt' } ],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			// fileEvents: workspace.createFileSystemWatcher( '**/.clientrc' )
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'mvtLanguageClient',
-		'MVT Language Client',
-		serverOptions,
-		clientOptions
+	context.subscriptions.push(
+		doValueCompletionProvider
 	);
-
-	// Start the client. This will also launch the server
-	client.start();
 
 }
 
 export function deactivate(): Thenable<void> | undefined {
 
-	if ( !client ) {
-		return undefined;
-	}
-
-	return client.stop();
+	return;
 
 }
