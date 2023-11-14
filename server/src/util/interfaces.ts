350b6e14af4e9ea15e41ac674376d90b77cd84c0
Max Hegler <mhegler@miva.com>
2023-11-14 12:06:04 -0800

    removed unused interface

--- server/src/util/interfaces.ts
+++ server/src/util/interfaces.ts
@@ -202,9 +202,3 @@ export interface SystemVariableData extends BaseSystemVariableData {
 	detail: string;
 	documentation: string;
 }
-
-export interface MvtVariableInfo {
-	value: string;
-	parts: string[];
-	depth: number;
-};
