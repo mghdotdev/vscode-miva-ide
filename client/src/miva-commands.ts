adefb30f2b59d986e6616c32cade3e9a26fcdc01
Max Hegler <mhegler@miva.com>
2023-12-15 11:26:21 -0800

    switched back to keybindings because vscode has no public API to actually get document contexts...

--- client/src/miva-commands.ts
+++ client/src/miva-commands.ts
@@ -1,5 +1,4 @@
 import { CharacterPair, Position, Range, TextEditor, TextEditorEdit, Uri, commands, env, languages, window, workspace } from 'vscode';
-import { Disposable } from 'vscode-languageclient';
 import patterns from './util/patterns';
 
 const boundryAmount = 200;
@@ -331,28 +330,15 @@ const calculatePosNumberCommand = commands.registerTextEditorCommand( 'mivaIde.M
 
 });
 
-// Hack that forces suggest window to appear after pasting
-
-const overwriteClipboardPasteAction = (textEditor: TextEditor) => {
-	// Dispose of overwritten command
-	overwriteClipboardPasteCommand.dispose();
-
+/**
+ * Decorated clipboard paste action that opens suggest window on paste
+ */
+const clipboardPasteCommand = commands.registerTextEditorCommand('mivaIde.clipboardPasteAction', () => {
 	// Execute original command
 	commands.executeCommand('editor.action.clipboardPasteAction').then(() => {
-		// Trigger suggestion popover if language matches
-		if (allowedLanguages.includes(textEditor.document.languageId)) {
-			commands.executeCommand('editor.action.triggerSuggest');
-		}
-
-		// Redefine overwritten command
-		defineOverwriteClipboardPasteCommand();
+		commands.executeCommand('editor.action.triggerSuggest');
 	});
-};
-const defineOverwriteClipboardPasteCommand = () => {
-	overwriteClipboardPasteCommand = commands.registerTextEditorCommand('editor.action.clipboardPasteAction', overwriteClipboardPasteAction);
-}
-let overwriteClipboardPasteCommand: Disposable;
-defineOverwriteClipboardPasteCommand();
+});
 
 export default [
 	chooseFileNameCommand,
@@ -362,5 +348,5 @@ export default [
 	convertToVariableCommand,
 	insertHtmlComment,
 	calculatePosNumberCommand,
-	overwriteClipboardPasteCommand
+	clipboardPasteCommand
 ];
