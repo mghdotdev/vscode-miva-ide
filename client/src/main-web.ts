5552d47ebbda8f92255911e7c471f689acd82438
Max Hegler <mhegler@miva.com>
2023-12-27 10:15:03 -0800

    uncommented language server client code

--- client/src/main-web.ts
+++ client/src/main-web.ts
@@ -1,30 +1,31 @@
-import { ExtensionContext } from 'vscode';
-import { sharedActivate } from './main-shared';
+import { ExtensionContext, Uri } from 'vscode';
+import { LanguageClient } from 'vscode-languageclient/browser';
+import { getClientOptions, sharedActivate } from './main-shared';
 
 // Define language client to be used during activate and deactivate callbacks
-// let client: LanguageClient;
+let client: LanguageClient;
 
 export function activate (context: ExtensionContext) {
 	// Path to server module
-	// const serverMain = Uri.joinPath(context.extensionUri, 'server/out-web/main.js');
+	const serverMain = Uri.joinPath(context.extensionUri, 'server/out-web/main.js');
 
 	// Create server worker
-	// const worker = new Worker(serverMain.toString(true));
+	const worker = new Worker(serverMain.toString(true));
 
-	// const clientOptions = getClientOptions();
+	const clientOptions = getClientOptions();
 
 	// Create the language client and start the client.
-	// client = new LanguageClient('miva', 'Miva IDE', clientOptions, worker);
-	// client.registerProposedFeatures();
-	// client.start();
+	client = new LanguageClient('miva', 'Miva IDE', clientOptions, worker);
+	client.registerProposedFeatures();
+	client.start();
 
-	// context.subscriptions.push( client );
+	context.subscriptions.push( client );
 
 	sharedActivate(context);
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	// client?.stop();
+	client?.stop();
 
 	return;
 }
