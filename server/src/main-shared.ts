6baa3c56b129265cbaefd3f4ce74f75169241d0a
Max Hegler <maxghegler@gmail.com>
2024-05-29 11:20:03 -0700

    added diagnostic validation on save of the document

--- server/src/main-shared.ts
+++ server/src/main-shared.ts
@@ -374,9 +374,16 @@ export function activate (connection: Connection, {workspaceSymbolProvider, miva
 
 	// The content of a text document has changed. This event is emitted
 	// when the text document first opened or when its content has changed.
-	documents.onDidChangeContent(change => {
+	documents.onDidChangeContent(changeEvent => {
 
-		triggerValidation( change.document );
+		triggerValidation( changeEvent.document );
+
+	});
+
+	// The text document was saved. This event is emitted
+	documents.onDidSave(changeEvent => {
+
+		triggerValidation( changeEvent.document );
 
 	});
 
